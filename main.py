d52b9a6a382f15ab7b6d93e847b0c43299539dd4
jofg123 <78727674+jofg123@users.noreply.github.com>
2021-02-12 07:33:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -438,9 +438,8 @@ def main():
 	updater.dispatcher.add_handler(MessageHandler(False, msg))
 	updater.dispatcher.add_error_handler(error_callback)
 
-        intervention_job = job(divine_intervention, 3 * 60 * 60.0)
+     
 	update_tornament_job = Job(update_tornament, 10.0)
-	updater.job_queue.put(intervention_job)
 	updater.job_queue.put(update_tornament_job)
 
 	queue_job = job(queue_reply, 0.035)
